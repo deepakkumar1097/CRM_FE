d2ee3681f7ce257afa51b7731aef4f0678c31206
Deepak <itisdeepakkumar@hotmail.com>
2023-10-09 15:45:37 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,51 @@
-import React from "react";
-import { AppBar, Grid, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Button,
+  Grid,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-function Navbar() {
+function Navbar({ links }) {
+  const [value, setValue] = useState();
+
   return (
-    <AppBar>
+    <AppBar sx={{ backgroundColor: "black" }}>
       <Toolbar>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} alignItems="baseline">
           <Grid item xs={2}>
             <Typography>Dashboard</Typography>
           </Grid>
-          <Grid item xs={5}>
-            <Tabs value={0} textColor="inherit" indicatorColor="secondary">
-              <Tab value="one" label="Item One" />
-              <Tab value="two" label="Item Two" />
-              <Tab value="three" label="Item Three" />
+          <Grid item xs={7}>
+            <Tabs
+              value={value}
+              textColor="inherit"
+              indicatorColor="secondary"
+              onChange={(e, val) => setValue(val)}
+            >
+              {links.map((item, index) => (
+                <Tab label={item} key={index} />
+              ))}
             </Tabs>
+          </Grid>
+          <Grid item xs={1} />
+          <Grid item xs={2}>
+            <Box>
+              <Button sx={{ marginLeft: "auto" }} variant="primary">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: 1 }} variant="primary">
+                Sign Up
+              </Button>
+            </Box>
           </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
   );
 }
-
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
+const linksArray = ["Item One", "Item Two", "Item Three", "Item Four"];
+
 function Home() {
   return (
     <div className="home">
-      <Navbar />
+      <Navbar links={linksArray} />
     </div>
   );
 }
